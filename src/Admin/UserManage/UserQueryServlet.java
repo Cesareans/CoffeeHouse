40b1bf8f2ac7d275710cf4b0127009c1a4f3c601
cesare <1357959025@qq.com>
2018-07-05 10:22:43 +0800

    Admin Backstage - Member

--- src/Admin/UserManage/UserQueryServlet.java
+++ src/Admin/UserManage/UserQueryServlet.java
@@ -1,8 +1,8 @@
 package Admin.UserManage;
 
 import Database.DBUser;
-import Debug.Debug;
 import Entity.User;
+import com.alibaba.fastjson.JSON;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,10 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.*;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import net.sf.json.*;
+import java.util.Map;
 
 @WebServlet(name = "UserQueryServlet")
 public class UserQueryServlet extends HttpServlet {
@@ -26,7 +27,7 @@ public class UserQueryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request,response);
     }
-    private void processRequest(HttpServletRequest request , HttpServletResponse response){
+    private void processRequest(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException{
         int start;
         int count;
         try {
@@ -40,8 +41,13 @@ public class UserQueryServlet extends HttpServlet {
             DBUser dbUser = new DBUser();
             userList = dbUser.getAllUsers();//未考虑效率
         }
-        JSONObject jsonObject = new JSONObject();
 
+        Map<String , Object> jsonMap = new HashMap<>();
+        jsonMap.put("userList" , userList);
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.write(JSON.toJSONString(jsonMap));
+        pw.close();
     }
     private List<User> getUsers(int start , int count){
         if(start < 0)
@@ -52,12 +58,5 @@ public class UserQueryServlet extends HttpServlet {
             return userList.subList(start,userList.size());
         else
             return userList.subList(start,start+count);
-    }
-
-    public static void main(String[] args) {
-        DBUser dbUser = new DBUser();
-        ArrayList<User> userList = dbUser.getAllUsers();//未考虑效率
-
-        System.out.println(JSONSerializer.toJSON(userList));
     }
 }
